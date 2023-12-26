1ca166e05f67a48a27767f7e17b97a1f043782c9
Rui Fernandes <rfernandes@confluent.io>
2023-12-26 17:46:44 +0000

    filter options to present only from last 3 months and from last 6 onths

--- clientside.js
+++ clientside.js
@@ -4,8 +4,20 @@ var newWindow;
 var subdomains=getSubDomains(document.URL);
 var token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 var startURL=subdomains.join('');
+var fromDate=new Date(0);
+var allButton=null;
+var months3Button=null;
+var months6Button=null;
+var infoMessage=null;
+var selectedStyle="background: #000000;color: #ffffff;padding: 0.5rem 1rem;border-radius: 0.5rem;"+
+                    "cursor: hand;font-family: Arial, Helvetica, sans-serif;font-size: 14px;margin-top: 10px;"+
+                       "margin-bottom: 10px;margin-right: 10px;margin-left: 10px;";
+var unselectedStyle="background: #A5D6A7;color: #ffffff;padding: 0.5rem 1rem;border-radius: 0.5rem;"+
+                    "cursor: hand;font-family: Arial, Helvetica, sans-serif;font-size: 14px;margin-top: 10px;"+
+                       "margin-bottom: 10px;margin-right: 10px;margin-left: 10px;";
+var totalSize=0;
 
-async function getTicket(ticketId,_callback) {
+async function getTicket(ticketId,ticketDate,_callback) {
     fetch(startURL+"/tickets/"+ticketId+"/print", {
         method: 'GET',
         credentials: 'include',
@@ -13,9 +25,9 @@ async function getTicket(ticketId,_callback) {
                 'X-CSRF-Token': token
             }
     }).then(function(responseTicket) {
-        printToWindow( 'Ticket details '+ticketId);
+        infoToWindow( 'Ticket details '+ticketId);
         responseTicket.text().then( (html) => {
-            tickets.set(ticketId,html);
+            tickets.set(ticketDate,html);
             if(_callback){
                 _callback();
             }
@@ -24,40 +36,77 @@ async function getTicket(ticketId,_callback) {
 }
 
 async function getTickets(orgId,_callback){
-    printToWindow( 'FETCHING...');
-    fetch(startURL+"/api/v2/views/preview.json?_include_archive=true&per_page=300&page=1", {
-        method: 'POST',
-        credentials: 'include',
-        headers: {
-                'Content-Type': 'application/json',
-                'X-CSRF-Token': token
-            },
-        body: '{"view":{"all":[{"field":"organization","operator":"is","value":'+orgId+'}],"output":{"columns":["status","id","subject","requester","created","updated","group","assignee"],"group_by":"status","group_order":"asc","sort_by":"updated","sort_order":"desc"}}}'
-    }).then(function(response) {
-        response.clone().json().then((json) => {
-            var ticketRows=json.rows;
-            ticketsLength=ticketRows.length;
-            printToWindow( 'Tickets length '+ticketsLength);
-            for(let i=0;i<ticketRows.length;i++) {
-                var ticketId=ticketRows[i].ticket.id;
-                getTicket(ticketId,_callback);
-            }
-        })
+    infoToWindow( 'FETCHING...');
+    if(tickets.size==0) {
+        fetch(startURL+"/api/v2/views/preview.json?_include_archive=true&per_page=300&page=1", {
+                method: 'POST',
+                credentials: 'include',
+                headers: {
+                        'Content-Type': 'application/json',
+                        'X-CSRF-Token': token
+                    },
+                body: '{"view":{"all":[{"field":"organization","operator":"is","value":'+orgId+'}],"output":{"columns":["status","id","subject","requester","created","updated","group","assignee"],"group_by":"status","group_order":"asc","sort_by":"updated","sort_order":"desc"}}}'
+            }).then(function(response) {
+                response.clone().json().then((json) => {
+                    var ticketRows=json.rows;
+                    ticketsLength=ticketRows.length;
+                    infoToWindow( 'Tickets length '+ticketsLength);
+                    for(let i=0;i<ticketRows.length;i++) {
+                        var ticketId=ticketRows[i].ticket.id;
+                        var ticketDate=new Date(ticketRows[i].created);
+                        getTicket(ticketId,ticketDate,_callback);
+                    }
+                })
 
-    });
+            });
+    } else {
+        printTicketsIfComplete();
+    }
+
+}
+
+function setLabel(id,innerHtml,onclickFunction){
+    var labelNode=newWindow.document.body.appendChild(document.createElement('label'));
+    labelNode.id=id;
+    labelNode.innerHTML=innerHtml;
+    labelNode.style = unselectedStyle;
+    labelNode.onclick=onclickFunction;
+    return labelNode;
+}
+
+function setupBody(){
+    newWindow.document.body.innerHTML = '';
+    allButton=setLabel('all','All Tickets',getAll);
+    months3Button=setLabel('3months','Last 3 months',get3Months);
+    months6Button=setLabel('6months','Last 6 months',get6Months);
+    infoMessage= newWindow.document.body.appendChild(document.createElement('div'));
+    infoMessage.style='margin-top: 10px;margin-bottom: 10px;margin-right: 10px;margin-left: 10px;';
 }
 
 
 function printToWindow(msg) {
-    newWindow.document.body.innerHTML = null;
-    newWindow.document.writeln(msg);
+    newWindow.document.body.insertAdjacentHTML("beforeend",msg);
+}
+
+function infoToWindow(msg) {
+    infoMessage.innerHTML=msg;
+}
+
+function filterDate(key) {
+    console.log(key);
+    return key.getTime() > fromDate.getTime();
 }
 
 
 function printTicketsIfComplete(){
     if(tickets.size==ticketsLength) {
-        tickets = new Map([...tickets.entries()].sort((a, b) => b[0] - a[0]));
-        printToWindow(Array.from(tickets.values()).join(''));
+        infoMessage.innerHTML='';
+        var tickets2 = new Map(
+                         [...tickets]
+                         .filter((entry) => filterDate(entry[0]) ));
+        tickets2 = new Map([...tickets2.entries()].sort((a, b) => b[0] - a[0]));
+        infoToWindow("Total Tickets: "+tickets2.size);
+        printToWindow(Array.from(tickets2.values()).join(''));
     }
 }
 
@@ -67,13 +116,40 @@ function getSubDomains(str){
     results = results.concat(str.match(/\.\w+/g));
     return results;
 }
+
+function fetchTickets(){
+    getTickets(orgId,printTicketsIfComplete);
+}
+
+function getAll() {
+    fromDate = new Date(0);
+    setupBody();
+    allButton.style=selectedStyle;
+    fetchTickets();
+}
+
+function get3Months() {
+    fromDate = new Date();
+    fromDate.setMonth(fromDate.getMonth() - 3);
+    setupBody();
+    months3Button.style=selectedStyle;
+    fetchTickets();
+}
+
+function get6Months() {
+    fromDate = new Date();
+    fromDate.setMonth(fromDate.getMonth() - 6);
+    setupBody();
+    months6Button.style=selectedStyle;
+    fetchTickets();
+}
+
 var startOrgUrl='https://'+subdomains[1]+'.zendesk.com/agent/organizations/';
 if(document.URL.startsWith(startOrgUrl)) {
     newWindow = window.open('','chrome-extension-zendesk');
-    newWindow.document.body.innerHTML = null;
     var lastPart=document.URL.substring(startOrgUrl.length);
     var orgId=lastPart.substring(0,lastPart.indexOf('/'));
-    getTickets(orgId,printTicketsIfComplete);
+    getAll();
 } else {
     //do nothing
 
